6daa5f35d3aa405de0f29155fc9d87219f91d704
Diba Makki <sdmakki@myseneca.ca>
2024-07-22 16:33:14 -0400

    directory changed

--- PersonalPotfolio/utils/constants.js
+++
@@ -1,6 +0,0 @@
-const botToken = process.env.TELEGRAM_BOT_TOKEN;
-const telegramChatId = process.env.TELEGRAM_CHAT_ID;
-
-const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
-
-module.exports = { telegramUrl, telegramChatId };
